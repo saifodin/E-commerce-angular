fda2e359ff30ddb3cf0c24d9b320df92a969d987
Mohamed Matouk <mmmatouk96@gmail.com>
2022-04-07 19:36:11 +0200

    login update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,35 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
-UserInfo : any
-  constructor(private _AuthService:AuthService)  {
-    this.UserInfo=JSON.parse(localStorage.getItem('userInfo')!)
-  _AuthService.currentUser.subscribe(()=>{
+  userData : any;
 
-    if(_AuthService.currentUser.getValue()!=null)
-    {
-      this.isLogin = true;
-    }
-    else
-    {
-      this.isLogin =false;
-    }
-  })
-   }
-isLogin:boolean= false;
-isLogOut()
-{
-  this._AuthService.logout()
-}
-  ngOnInit(): void {
+  constructor(private _AuthService: AuthService, private http: HttpClient) {
+   
+    _AuthService.currentUser.subscribe(() => {
+      if (_AuthService.currentUser.getValue() != null) {
+        this.isLogin = true;
+      } else {
+        this.isLogin = false;
+      }
+    });
+  }
+  isLogin: boolean = false;
+  isLogOut() {
+    this._AuthService.logout();
   }
 
+  private getUserInfo(){
+    this.http
+      .get<any[]>(`https://eshop-iti.herokuapp.com/api/v1/users/get/me`)
+      .subscribe((response:any) => {
+        console.log(response);
+        this.userData = response
+        console.log(this.userData.name)
+      });
+  }
+
+  ngOnInit(): void {
+
+    this.getUserInfo();
+  }
 }
